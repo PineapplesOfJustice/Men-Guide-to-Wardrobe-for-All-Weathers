bd1474516667d56abab028cdd50c6484ef66cb65
Khai Sam <Gre8nThu7der@gmail.com>
2018-05-01 19:32:11 -0700

    Initial commit

---
+++ wardrobe.js
@@ -0,0 +1,208 @@
+
+var shirtCold = ["https://www.fjackets.com/product_images/e/008/Arsenal_Red_Arrow_Hooded_Jacket__37695_zoom.jpg", 
+                 "https://i.ebayimg.com/thumbs/images/g/XF4AAOSwbsBXo0rH/s-l225.jpg", 
+                 "https://images.thenorthface.com/is/image/TheNorthFace/NF0A37PI_15Q_hero?$638x745$", 
+                 "http://www.tshirt-factory.com/images/detailed/20/NEED-SUMMER-T-shirt-design-20667.jpg", 
+                 "https://ae01.alicdn.com/kf/HTB1NSRbIXXXXXawXXXXq6xXFXXXq/100-Cotton-T-Shirt-Men-Evolution-Table-Tennis-Boys-TShirts-Design-Your-Own-O-Neck-Tee.jpg_640x640.jpg", 
+                 "https://fashiongossiplondon.com/ahxax/Images1/Shirts/thickLumberJack/checkred1.jpg", 
+                 "https://res.cloudinary.com/teepublic/image/private/s--DMtmSdL1--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_461/c_crop,g_north_west,h_626,w_470,x_-5,y_-32/g_north_west,u_upload:v1462829024:production:blanks:a59x1cgomgu5lprfjlmi,x_-400,y_-357/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1478105592/production/designs/766848_1.jpg", 
+                 "https://images.thenorthface.com/is/image/TheNorthFace/CL72_JK3_hero?$638x745$",
+                 "https://images-na.ssl-images-amazon.com/images/I/71z23xu59HL._UX385_.jpg"];
+var shirtWarm = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcxYl9RyaYFHUjOjR9cg-VqOjHfViYUdQ8bbRAFpnV1cNuTAMxxg",
+                 "https://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=20038440",
+                 "https://s7.ralphlauren.com/is/image/PoloGSI/s7-1263158_lifestyle?$rl_392_pdp$",
+                 "http://resources.cungmua.com/ftpproduct/12183/ao-so-mi-nam-tay-ngan-mau%20(8).jpg", 
+                 "https://skreened.com/render-product/p/y/q/pyqmcqtcdamqcxgpvnrg/image.skreened-t-shirt.white.w460h520b3z1.jpg",
+                 "http://groupon.s3.amazonaws.com/sponsorship-imgs/Goods/Solid%20Threads/gs_men.jpeg",    "http://www.fanrek.com/media/catalog/product/cache/1/image/29b82db22504fb0479cf1248d4fc542c/s/t/star_trek_tos_engineering_red_shirt_uniform_costume_1_.jpg" ];
+var pantCold = ["https://sep.yimg.com/ay/policestuff/la-police-gear-atlas-tactical-pant-with-sts-196.gif", 
+                "https://cdn.ccs.com/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/v/o/volcom-freakin-snow-chino-pant-2015-khaki_5.1506837977.jpg", 
+                "http://www.allsportslex.com/istarimages/mp/BR2159!001-21695_d.jpg",
+                "https://riverisland.scene7.com/is/image/RiverIsland/299770_main?wid=1200",
+                "https://img2.newchic.com/thumb/large/oaupload/newchic/images/7E/7E/c7d18ce0-f09c-429b-86ab-d0e9a9b36846.jpg",
+                "http://scene7.zumiez.com/is/image/zumiez/pdp_hero/Crysp-Denim-Bobby-Ink-Splatter-Ripped-Jeans-_271929-back.jpg",];
+var pantWarm = ["http://scene7.zumiez.com/is/image/zumiez/pdp_hero/Free-World-Remy-Dark-Khaki-Jogger-Pants-_259942-front.jpg",     "https://media.quiksilver.com.au/media/catalog/product/cache/image/1000x1000/9df78eab33525d08d6e5fb8d27136e95/e/q/eqyws03252_quiksilver_mens_everyday_chino_walkshort_tmp0_1_h.jpg",
+                "http://lp.hm.com/hmprod?set=source%5B%2Fmodel%2F2016%2FA00%200183311%20002%2006%202257.jpg%5D%2Cmedia_type%5BSTILL_LIFE_FRONT%5D%2Ctshirt_size%5BL%5D%2Cquality%5BH%5D%2Chmver%5B0%5D&call=url%5Bfile%3A%2Flegacy%2Fv1%2Fproduct.chain%5D",
+                "https://contents.mediadecathlon.com/p731004/460x460/sq/mens_swimshort_190_long_stril_blue_nabaiji_8361410_731004.jpg?k=11a45e24ac20a8e867d11047477739ef",
+                "https://s7d2.scene7.com/is/image/dkscdn/15CTTMRGGDCRGKHKCAPB_Rugged_Grey_Camo_is",
+                "https://belk.scene7.com/is/image/Belk?layer=0&src=3202407_JSENPTSP0000_A_001_T10L00&layer=comp&$DWP_PRODUCT_ZOOM_DESKTOP$",];
+var shoeCold = ["https://m.media-amazon.com/images/I/819V1GsmslL._AC_SX255_.jpg",
+                "https://www.dhresource.com/0x0s/f2-albu-g5-M00-40-98-rBVaI1lH9kGAJ1rzAABP20-_qlk784.jpg/wholesale-2016-men-fashion-elastic-band-non.jpg",
+                "http://www.zappos.com/images/z/3/5/8/7/5/2/3587522-p-2x.jpg", 
+                "https://images-na.ssl-images-amazon.com/images/I/71ipQUl-44L._UY395_.jpg", 
+                "https://image.dhgate.com/0x0/f2/albu/g3/M01/73/54/rBVaHVaDl9aALMAmAALiRwBVAgs888.jpg", ];
+var shoeWarm = ["https://cdn.riastatic.com/photosnew/general/adv_photos/kozhanye_muzhskye_bosonozhky_won_the_original__31605654m.jpg",
+                "https://rukminim1.flixcart.com/image/832/832/j7td5e80/sandal/5/w/b/202965-001-43-5-crocs-1-original-imaexyxapmtcgaad.jpeg?q=70",
+                "https://www.brostrick.com/wp-content/uploads/2016/03/vans-shoes-men-black-checkerboard-2017-2018-e1494516374914.jpg",
+                "http://www.nikeairmax-1.com/wp-content/uploads/2015/09/Tennis-shoe.jpg",
+                "https://www.dhresource.com/0x0s/f2-albu-g4-M00-FF-20-rBVaEVb6FA-AB8ObAAGdFbv1vJk484.jpg/nmd-city-sock-s79150-men-and-women-shoe-nmd.jpg",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlLaz-VXEcvV8HEyB8VBazbfClTZPu2qk_u7IRWX1wDtDKZdAA",];
+var shoeRain = ["https://images-na.ssl-images-amazon.com/images/I/41Mi0RSeAbL.jpg",
+                "https://sc01.alicdn.com/kf/HTB1_gTHXBUSMeJjSszcq6znwVXaa/Men-Oiled-Field-Water-Rain-Boots-Kee.jpg_350x350.jpg",
+                "http://www.olixe.com/wp-content/uploads/2013/06/Boots-For-Men-Tretorn-Gunnar.jpg", 
+                "https://images-na.ssl-images-amazon.com/images/I/81cAWudO86L._UL1500_.jpg",];
+var normalMiss = ["https://psm3.s3.amazonaws.com/1490770249_1fc745cd11df05a4baa84605954dae92.png", 
+                  "https://img0.etsystatic.com/212/0/7168179/il_340x270.1224920142_hi52.jpg",
+                  "https://shop.r10s.jp/mituisi/cabinet/02387423/img58982911.jpg",
+                  "https://images-na.ssl-images-amazon.com/images/I/81Y636YrxEL._SX355_.jpg",
+                  "https://store.haloheadband.com/v/vspfiles/photos/SV-2.jpg",
+                  "https://cdn.shopify.com/s/files/1/0445/0437/products/beanie-smoke-speckled-pom-beanie-black-pom-1.jpg?v=1506021578",
+                  "https://www.visionworks.com/images/product/1312939/1312939sunb_lg.jpg",
+                  "https://cdn.mec.ca/medias/sys_master/fallback/fallback/8813523140638/5041377-GRY13-fallback.jpg",
+                  "http://stlfitnessbootcamp.com/wp-content/uploads/2015/11/imposter.jpg",
+                  "https://images-na.ssl-images-amazon.com/images/I/51q1dHmGj1L._SY300_.jpg",                
+                  "https://images-na.ssl-images-amazon.com/images/I/41mjzo4CMuL._SL500_AC_SS350_.jpg",
+                  "http://www.patagonia.com/dis/dw/image/v2/ABBM_PRD/on/demandware.static/-/Sites-patagonia-master/default/dwe18bb533/images/hi-res/83685_BLK.jpg?sw=750&sh=750&sm=fit&sfrm=png", 
+                  "https://img.newfrog.com/products/1/0/0/100685.01/100685.01-1.jpg",
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6cGB58s5PpXeYJIpEIXwWF6BxMCwzAhk0abaYAKyE829WLS3F",
+                  "https://images-na.ssl-images-amazon.com/images/I/716FZT8nSyL._UL1500_.jpg",];
+var rainMiss = ["https://www.hawkin.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/e/3/e38638.jpg",
+                "https://inhabitat.com/ecouterre/wp-content/blogs.dir/26/files/2013/08/wearable-shelter-lucy-orta-refugee-wear.jpg", 
+                "https://shop.r10s.jp/mituisi/cabinet/02387423/img58982911.jpg",
+                "https://gazettereview.com/wp-content/uploads/2017/07/Umbrella.jpg", 
+                "http://www.ikea.com/us/en/images/products/knalla-rain-poncho-white__0441604_PE593468_S4.JPG",
+                "https://psm3.s3.amazonaws.com/1490770249_1fc745cd11df05a4baa84605954dae92.png",];
+var snowMiss = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk3boChM1n4rQXqHA8_hjzQRHyym59kSdiSIb3fQ__A9nEp8TL", 
+                "https://images-na.ssl-images-amazon.com/images/I/71Cx5YGX18L._UX562_.jpg", 
+                "https://images-na.ssl-images-amazon.com/images/I/51bedRZtF3L.jpg",
+                "https://psm3.s3.amazonaws.com/1490770249_1fc745cd11df05a4baa84605954dae92.png",
+                "https://inhabitat.com/ecouterre/wp-content/blogs.dir/26/files/2013/08/wearable-shelter-lucy-orta-refugee-wear.jpg",  ];
+var windMiss = ["http://images.costumeexpress.com/mgen/alice-in-wonderland-classic-tweedledee-dum-hat-adult-cx-19480.jpg?zm=800,800,1,0,0",
+                "http://scene7.zumiez.com/is/image/zumiez/pdp_hero/RVCA-Bay-Breaker-Grey-Windbreaker-Jacket-_189948-0025-front.jpg",
+                "https://images-na.ssl-images-amazon.com/images/I/41mjzo4CMuL._SL500_AC_SS350_.jpg",
+                "https://psm3.s3.amazonaws.com/1490770249_1fc745cd11df05a4baa84605954dae92.png",];
+
+var temperature = "";
+var weather = "";
+var wind = false;
+var dataUpdated = false;
+
+var shirt = "";
+var pant = "";
+var shoe = "";
+var accesory = "";
+
+function wardSuggester(data){
+  dataUpdated = true;  
+  document.getElementById("zipcode").setAttribute("placeholder", "City Name or Zipcode");
+  document.getElementById("refresh").setAttribute("style", "display: block;");
+  document.getElementById("outfit").innerHTML = "<br><span id='green'>City: </span>" + data.location.name + "<br>";
+  document.getElementById("outfit").innerHTML += "<span id='green'>Region: </span>" + data.location.region + "<br>";
+  document.getElementById("outfit").innerHTML += "<span id='green'>Country: </span>" + data.location.country + "<br>";
+  document.getElementById("outfit").innerHTML +=  "<span id='green'>Temperature: </span>" + data.current.temp_f + "°F (" + data.current.temp_c + "°C)<br>";     
+  document.getElementById("outfit").innerHTML += "<span id='green'>Weather: </span>" + data.current.condition.text + "<br>";  document.getElementById("outfit").innerHTML +=  "<span id='green'>Wind: </span>" + data.current.wind_mph + " mph (" + data.current.wind_kph + " kph)<br><br>";    
+  updateData(data);  
+  pickOutfit();
+  pickBackground();  
+}
+
+function updateData(data){
+    //Temperature
+  if(data.current.temp_f < 70){ temperature="cold"; }
+  else if(data.current.temp_f < 85){ temperature="warm"; }
+  else{ temperature="hot"; } 
+    //Weather
+  if(data.current.condition.text.includes("Rain") || data.current.condition.text.includes("rain")){ weather="rain"; }
+  else if(data.current.condition.text.includes("Snow") || data.current.condition.text.includes("snow")||data.current.condition.text.includes("Hail") || data.current.condition.text.includes("hail")){ weather="ice"; }
+  else{ weather="fine"; }  
+    //Wind
+  if(data.current.wind_mph < 12){ wind=false; }
+  else{ wind=true; } 
+}
+
+function pickBackground(){
+  if(weather == "rain" && wind == true){ document.body.style.backgroundImage = "url('https://untappedcities-wpengine.netdna-ssl.com/wp-content/uploads/2013/05/rain-room-nyc-untapped-cities-jane-hu-2.jpg')"; }
+  else if(weather == "ice" && wind == true){ document.body.style.backgroundImage = "url('http://www.slate.com/content/dam/slate/blogs/the_slatest/2016/01/21/all_your_east_coast_blizzard_questions_answered/160121_slatest_blizzard.jpg.CROP.promo-xlarge2.jpg')"; }
+  else if(weather == "rain"){ document.body.style.backgroundImage = "url('https://www.myjewishlearning.com/wp-content/uploads/2002/09/rain-umbrella.jpg')"; }
+  else if(weather == "ice"){ document.body.style.backgroundImage = "url('http://www.insidesocal.com/snow/files/2014/02/Spring-Skiing-at-BBMR.jpg')"; }
+  else if(wind == true){ document.body.style.backgroundImage = "url('https://www.worldatlas.com/r/w728-h425-c728x425/upload/09/1c/25/shutterstock-353409533.jpg')"; }
+  else if(temperature == "cold"){ document.body.style.backgroundImage = "url('http://apparentlynothing.com/wp-content/uploads/2008/12/wintertree.jpg')"; }
+  else if(temperature == "warm"){ document.body.style.backgroundImage = "url('https://singles-cdn.s3.amazonaws.com/wp-content/uploads/2016/11/GALLERY-Mekong-River-Cruise-boats.jpg')"; }  
+  else if(temperature == "hot"){ document.body.style.backgroundImage = "url('https://fthmb.tqn.com/XN77_47-LyAdTUa2S4MOvWGpolw=/2126x1412/filters:fill(auto,1)/white_sand_beach-56d50c955f9b5879cc92cc5c.jpg')"; }  
+  else{ document.body.style.backgroundImage = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8TLxP796ABE1pHNjHqAj8Mxy7v1QrDxyrWSIMWSoy6VYOVHdtxA')"; }  
+}
+
+function pickOutfit(){
+  if(weather == "rain" && wind == true){
+    if(Math.random() > 0.5){  accesory = windMiss[Math.floor(Math.random()*windMiss.length)]; }
+    else{ accesory = rainMiss[Math.floor(Math.random()*rainMiss.length)]; }
+    shoe = shoeRain[Math.floor(Math.random()*shoeRain.length)];  
+  }
+  else if(weather == "ice" && wind == true){
+    if(Math.random() > 0.5){  accesory = windMiss[Math.floor(Math.random()*windMiss.length)]; }
+    else{ accesory = snowMiss[Math.floor(Math.random()*snowMiss.length)]; }
+  }
+  else if(weather == "rain"){
+    accesory = rainMiss[Math.floor(Math.random()*rainMiss.length)];  
+    shoe = shoeRain[Math.floor(Math.random()*shoeRain.length)];
+  }
+  else if(weather == "ice"){
+    accesory = snowMiss[Math.floor(Math.random()*snowMiss.length)];   
+  }
+  else if(wind == true){
+    accesory = windMiss[Math.floor(Math.random()*windMiss.length)];  
+  }
+  if(temperature == "cold"){
+    if(weather != "rain" && weather != "ice" && wind == false){ accesory = normalMiss[Math.floor(Math.random()*normalMiss.length)]; }
+    shirt = shirtCold[Math.floor(Math.random()*shirtCold.length)];
+    pant = pantCold[Math.floor(Math.random()*pantCold.length)];
+    if(weather != "rain"){ shoe = shoeCold[Math.floor(Math.random()*shoeCold.length)]; }
+  }
+  else if(temperature == "warm"){
+    if(weather != "rain" && weather != "ice" && wind == false){ accesory = normalMiss[Math.floor(Math.random()*normalMiss.length)]; }
+    shirt = shirtWarm[Math.floor(Math.random()*shirtWarm.length)];
+    pant = pantWarm[Math.floor(Math.random()*pantWarm.length)];
+    if(weather != "rain"){ shoe = shoeWarm[Math.floor(Math.random()*shoeWarm.length)]; }      
+  }  
+  else if(temperature == "hot"){
+    if(weather != "rain" && weather != "ice" && wind == false){ accesory = normalMiss[Math.floor(Math.random()*normalMiss.length)]; }
+    shirt = shirtWarm[Math.floor(Math.random()*shirtWarm.length)];
+    pant = pantWarm[Math.floor(Math.random()*pantWarm.length)];
+    if(weather != "rain"){ shoe = shoeWarm[Math.floor(Math.random()*shoeWarm.length)]; }
+  }  
+  document.getElementById("dress").innerHTML = "<img src='" + accesory + "'/>";
+  document.getElementById("dress").innerHTML += "<img src='" + shirt + "'/>";
+  document.getElementById("dress").innerHTML += "<img src='" + pant + "'/>";
+  document.getElementById("dress").innerHTML += "<img src='" + shoe + "'/> <br>";   
+}
+
+function makeRequest() {
+  if(Number(document.getElementById("zipcode").value) != 0){  
+    var url = "https://api.apixu.com/v1/current.json?key=0ca8b464bb0e4433b9b200308182202&q=" + document.getElementById("zipcode").value;
+    document.getElementById("zipcode").value = "";
+    console.log(url);  
+    setTimeout(ifNotUpdated, 1000);  
+    $.ajax({
+      url: url,
+      success: function(data) {
+        wardSuggester(data);
+      }
+    })
+  }
+}
+
+function ifNotUpdated(){
+  if(dataUpdated == false){ 
+    document.getElementById("zipcode").setAttribute("placeholder", "Location not Found");
+    document.getElementById("refresh").setAttribute("style", "display: none;");
+    document.getElementById("outfit").innerHTML = "";
+    document.getElementById("dress").innerHTML = ""; 
+    temperature = "";
+    weather = "";
+    wind = false;
+    pickBackground();  
+  }  
+  else{ dataUpdated = false; }   
+}
+
+document.addEventListener('keypress',hello)
+function hello(event) {       
+  if(event.key == "Enter"){
+    makeRequest();  
+  }
+}
+
+getCurrentAddress();
+function getCurrentAddress(){
+  document.getElementById("zipcode").value = geoplugin_city() + " " + geoplugin_region();  
+  makeRequest();  
+}
